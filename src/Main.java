4b39146988b60acc571d263e7140549c78bf6352
Palich63 <globa.andrey@gmail.com>
2023-01-23 02:21:16 +0300

    Изменено V00001

--- src/Main.java
+++ src/Main.java
@@ -13,110 +13,146 @@ public class Main {
 
     }
 }
-    class Calculate{
-    int result;
+    class Calculate {
+        int result;
+        int romTrue = 0;
+        String[] sim = {"+", "-", "/", "*"};
+        int x = 0, y = 0;
+        int simInd;
 
-        int peremennye(){
+        void peremennye() {
             Scanner vvod = new Scanner(System.in);
             String perem1;
-            int[] peremenn = new int[2];
+            String[] perem2;
             perem1 = vvod.nextLine(); //Ввод
-            perem1 = perem1.trim();
-            char sim = ' '; //Убрать пробел и создать исключение если ни одного из сиволов нет
-            String[] perem2 = new String[2];
-            if (perem1.contains("+")) {           // сложить
-                sim = '+';
-                perem2 = perem1.split("\\+");
-                for (int i = 0; i <= 1; i++) {
-                    perem2[i] = perem2[i].trim();
-                }
-            }
-            if (perem1.contains("-")) {               // отнять
-                sim = '-';
-                perem2 = perem1.split("-");
-                for (int i = 0; i <= 1; i++) {
-                    perem2[i] = perem2[i].trim();
-                }
-            }
-            if (perem1.contains("/")) {               // разделить
-                sim = '/';
-                perem2 = perem1.split("/");
-                for (int i = 0; i <= 1; i++) {
-                    perem2[i] = perem2[i].trim();
-                }
-            }
-            if (perem1.contains("*")) {               // умножить
-                sim = '*';
-                perem2 = perem1.split("\\*");
-                for (int i = 0; i <= 1; i++) {
-                    perem2[i] = perem2[i].trim();
-                }
-            }
-            int x = Integer.parseInt(perem2[0]);
-            int y = Integer.parseInt(perem2[1]);
-            if (x>=1 && x<=10 && y>=1 && y<=10) {
-                peremenn[0] = x;
-                peremenn[1] = y;
-            }
+            perem1 = perem1.trim();  //Убрать пробел и создать исключение если ни одного из символов нет
+            obrabotca(perem1);
 
-            switch (sim){
-                case '+':
-                    result = peremenn[0] + peremenn[1];
-                    break;
-                case '-':
-                    result = peremenn[0] - peremenn[1];
-                    break;
-                case '/':
-                    result = peremenn[0] / peremenn[1];
-                    break;
-                case '*':
-                    result = peremenn[0] * peremenn[1];
-                    break;
+            String[] sim1 = {"\\+", "-", "/", "\\*"};
+            for (int i = 0; i < sim.length; i++) {
+                if (perem1.contains(sim[i])) {
+                    simInd = i;
+                }
+            }
+            perem2 = perem1.split(sim1[simInd]);
+            for (int i = 0; i <= 1; i++) {
+                perem2[i] = perem2[i].trim();
+            }
+            //
+            if (romTrue == 1) {
+                x = romToInt(perem2[0]);
+                y = romToInt(perem2[1]);
+                result = calc(x,y);
+                if (result < 1) {
+                    try {
+                        throw new Exception();
+                    } catch (Exception e) {
+                        System.out.println("в римской системе нет отрицательных чисел");
+                        System.exit(0);
+                    }
+                }
+
+            }else {
+                if (Integer.parseInt(perem2[0]) > 10 && Integer.parseInt(perem2[1]) > 10 && Integer.parseInt(perem2[1]) < 1 && Integer.parseInt(perem2[1]) < 1) {
+                    System.out.println("Значение не входит в требуемый диапазон");
+
+                } else {
+                    x = Integer.parseInt(perem2[0]);
+                    y = Integer.parseInt(perem2[1]);
+                    result = calc(x, y);
+                }
+            }
+        }
+
+        int obrabotca(String s) {
+            String arab = "1_2_3_4_5_6_7_8_9_0";
+            String rom = "I_II_III_IV_V_VI_VII_VIII_IX_X";
+            String per = "+_-_*_/";
+            int k = 0, a = 0, p = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (arab.contains(s.substring(i, i + 1))) {
+                    k = k + 1;
+                }
+                if (rom.contains(s.substring(i, i + 1))) {
+                    a = a + 1;
+                    romTrue = 1;
+                }
+                if (per.contains(s.substring(i, i + 1))) {
+                    p = p + 1;
+                }
+            }
+            if (k != 0 && a != 0) {
+                try {
+                    throw new Exception();
+                } catch (Exception e) {
+                    System.out.println("используются одновременно разные системы счисления");
+                    System.exit(0);
+                }
+            }
+            if (p > 1) {
+                try {
+                    throw new Exception();
+                } catch (Exception e) {
+                    System.out.println("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                    System.exit(0);
+                }
+            }
+            if (p == 0) {
+                try {
+                    throw new Exception();
+                } catch (Exception e) {
+                    System.out.println("строка не является математической операцией");
+                    System.exit(0);
+                }
+            }
+            return romTrue;
+        }
+
+        int getArabian(char rom) {
+            if ('I' == rom) return 1;
+            else if ('V' == rom) return 5;
+            else if ('X' == rom) return 10;
+            return 0;
+        }
+
+        int romToInt(String s) {
+
+            int end = s.length() - 1;
+            char[] arr = s.toCharArray();
+            int arabian;
+            int result = getArabian(arr[end]);
+            for (int i = end - 1; i >= 0; i--) {
+                arabian = getArabian(arr[i]);
+
+                if (arabian < getArabian(arr[i + 1])) {
+                    result -= arabian;
+                } else {
+                    result += arabian;
+                }
+
+
             }
             return result;
+
         }
-    void obrabotca(String s){
-         // String kiril[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-        String kiril = "1_2_3_4_5_6_7_8_9_0";
-            //String arab[] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        String arab = "I_II_III_IV_V_VI_VII_VIII_IX_X";
-        String per = "+_-_*_/";
-        int k = 0, a = 0, p = 0;
-            for (int i=0; i<s.length(); i++) {
-                if(kiril.contains(s.substring(i,i+1))){
-                    k=k+1;
-                }
-                if(arab.contains(s.substring(i,i+1))){
-                    a=a+1;
-                }
-                if(per.contains(s.substring(i,i+1))){
-                    p=p+1;
-                }
-            }
-        if (k!=0 && a!=0){
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.out.println("используются одновременно разные системы счисления");
-                System.exit(0);
-            }
+
+        int calc (int x, int y){
+        switch(sim[simInd])
+        {
+            case "+":
+                result = x + y;
+                break;
+            case "-":
+                result = x - y;
+                break;
+            case "/":
+                result = x / y;
+                break;
+            case "*":
+                result = x * y;
+                break;
         }
-        if (p>1) {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.out.println("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-                System.exit(0);
-            }
-        }
-        if (p==0) {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.out.println("строка не является математической операцией");
-                System.exit(0);
-            }
-        }
+            return result;
     }
     }
 
